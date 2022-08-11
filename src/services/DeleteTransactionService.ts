df4c85e39b0eada3e1fec72b57c7fc99f7a9b266
Fellyp Santos <fellypsantos2011@gmail.com>
2022-08-11 13:59:33 -0300

    finished - all tests passed

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,19 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
+
+import Transaction from '../models/Transaction';
+import AppError from '../errors/AppError';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    if (!id) throw new AppError('Transaction identifier is missing.');
+
+    const transactionsRepository = getRepository(Transaction);
+
+    const transaction = await transactionsRepository.findOne(id);
+
+    if (!transaction) throw new AppError('Transaction does not exists.');
+
+    await transactionsRepository.remove(transaction);
   }
 }
 
